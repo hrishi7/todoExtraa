25872bec9ad2eefe54631982432abfa57af7afa8
Hrishikesh Baidya <hrishikeshbaidya7@gmail.com>
2020-03-01 13:53:30 +0530

    initial upload

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static( 'client/build' ));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path comment
     });
 }
 const port = process.env.PORT || 5000;
